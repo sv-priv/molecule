e76b4f1a895e57772f60048424c1df7737a30629
VelkoskiS <svelkoskis@polygon.technology>
2023-02-03 17:43:34 +0100

    whitelisting, revoking, encryption, ipfs, minting done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,59 +14,290 @@ export default function User() {
   const [university, setUniversity] = useState(null)
   const [patentId, setPatentId] = useState(null)
   const [institution, setInstitution] = useState(null)
+  const [brightlistAddress, setBrightlistAddress] = useState(null)
+  const [revokeAddress, setRevokeAddress] = useState(null)
 
-  if (window.ethereum) {
-    window.ethereum.on("accountsChanged", function (accounts) {
-      setUserAddress(accounts[0])
-      localStorage.setItem("userAddress", accounts[0])
+  const [encryptionKey, setEncryptionKey] = useState(null)
 
-      location.reload()
-    })
+  const contractAddress = "0xe8c894d0b2e70d5ad03e66e7e5b31d24a96ae83a"
+  const contractABI = [
+    {
+      inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        { indexed: true, internalType: "uint256", name: "id", type: "uint256" },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "bool",
+          name: "approved",
+          type: "bool",
+        },
+      ],
+      name: "ApprovalForAll",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        { indexed: true, internalType: "address", name: "to", type: "address" },
+        { indexed: true, internalType: "uint256", name: "id", type: "uint256" },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "_addressToBrightlist",
+          type: "address",
+        },
+      ],
+      name: "addToBrightlist",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "spender", type: "address" },
+        { internalType: "uint256", name: "id", type: "uint256" },
+      ],
+      name: "approve",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "counter",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      name: "getApproved",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "", type: "address" },
+        { internalType: "address", name: "", type: "address" },
+      ],
+      name: "isApprovedForAll",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "string", name: "_tokenURI", type: "string" }],
+      name: "mintToken",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "id", type: "uint256" }],
+      name: "ownerOf",
+      outputs: [{ internalType: "address", name: "owner", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_addressToRevoke", type: "address" },
+      ],
+      name: "revokeFromBrightlist",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "id", type: "uint256" },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "id", type: "uint256" },
+        { internalType: "bytes", name: "data", type: "bytes" },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "operator", type: "address" },
+        { internalType: "bool", name: "approved", type: "bool" },
+      ],
+      name: "setApprovalForAll",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
+      name: "supportsInterface",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "id", type: "uint256" }],
+      name: "tokenURI",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "id", type: "uint256" },
+      ],
+      name: "transferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ]
 
-    const changeNetwork = async ({ networkName, setError }) => {
-      try {
-        if (!window.ethereum) throw new Error("No crypto wallet found")
-        await window.ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [
-            {
-              ...networks[networkName],
-            },
-          ],
-        })
-      } catch (err) {
-        setError(err.message)
-      }
-    }
+  const accounts = window.ethereum.request({ method: "eth_requestAccounts" })
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
 
-    const [error, setError] = useState()
+  const walletAddress = accounts[0]
 
-    const handleNetworkSwitch = async (networkName) => {
-      setError()
-      await changeNetwork({ networkName, setError })
-    }
+  const signer = provider.getSigner(walletAddress)
+  const signerAddress = signer.getAddress()
 
-    const networkChanged = (chainId) => {
-      console.log({ chainId })
-    }
+  window.ethereum.on("accountsChanged", function (accounts) {
+    setUserAddress(accounts[0])
+    localStorage.setItem("userAddress", accounts[0])
 
-    useEffect(() => {
-      const chainId = parseInt(window.ethereum.chainId)
-      localStorage.setItem("chainId", chainId)
-      setUserAddress(localStorage.getItem("userAddress"))
+    location.reload()
+  })
 
-      window.ethereum.on("chainChanged", networkChanged)
-      return () => {
-        window.ethereum.removeListener("chainChanged", networkChanged)
-      }
-    }, [])
+  const moleculeContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  )
+
+  async function getTokenURI(tokenId) {
+    const tokenURI = await moleculeContract.tokenURI(0)
+    return tokenURI
   }
 
-  async function brightlist() {}
+  async function addToBrightlist(e, address) {
+    e.preventDefault()
+    const whitelisted = await moleculeContract.addToBrightlist(address)
+    console.log(whitelisted)
+  }
+  async function revokeFromBrightlist(e, address) {
+    e.preventDefault()
+    const removed = await moleculeContract.revokeFromBrightlist(address)
+    console.log(removed)
+  }
 
-  async function revoke() {}
+  useEffect(() => {
+    const chainId = parseInt(window.ethereum.chainId)
+    localStorage.setItem("chainId", chainId)
+    setUserAddress(localStorage.getItem("userAddress"))
+  }, [])
 
-  async function encrypt() {}
+  function encrypt(contractData) {
+    //get a random encrytion key
+    let r = (Math.random() + 1).toString(36).substring(7)
+    console.log("random", r)
+
+    const ciphertext = cryptoJS.AES.encrypt(contractData, r)
+    console.log("cipertext", ciphertext.toString())
+    return ciphertext.toString()
+  }
+
+  async function decrypt() {}
 
   async function uploadToIpfs() {}
 
@@ -87,11 +318,8 @@ export default function User() {
     //encrypt the data
     const stringMintObject = JSON.stringify(mintObject)
 
-    let r = (Math.random() + 1).toString(36).substring(7)
-    console.log("random", r)
-
-    const ciphertext = cryptoJS.AES.encrypt(stringMintObject, r)
-    console.log("cipertext", ciphertext.toString())
+    const encryptedContractData = encrypt(stringMintObject)
+    console.log("encrypted data", encryptedContractData)
 
     // put the ciphertext string to IPFS
 
@@ -127,7 +355,16 @@ export default function User() {
     }
 
     const ipfsAddTokenURI = await ipfs.add(JSON.stringify(tokenUri))
+
     console.log("token uri", ipfsAddTokenURI.path)
+
+    //  await moleculeContract.addToBrightlist(
+    // //   signer.getAddress()
+    // // )
+    addToBrightlist(signerAddress)
+
+    revoke
+    await moleculeContract.mintToken(ipfsAddTokenURI.path)
 
     // contract.mint(tokenAddress, tokenUri.cid)
 
@@ -135,6 +372,7 @@ export default function User() {
 
     //one of the suggested things would be, build a backend and store them there not in local storage.
     // map every user address to the tokenID and the key
+
     const bytes = cryptoJS.AES.decrypt(ciphertext.toString(), r)
     const plaintext = bytes.toString(cryptoJS.enc.Utf8)
     console.log("plaintext", plaintext)
@@ -151,6 +389,47 @@ export default function User() {
         {window.ethereum ? (
           <div>
             <div>
+              <h5>Brightlist an ethereum address</h5>
+              <form>
+                <input
+                  type="text"
+                  value={brightlistAddress || ""}
+                  required
+                  onChange={(e) => setBrightlistAddress(e.target.value)}
+                />
+                <div>
+                  <button
+                    className="ml-button"
+                    onClick={(e) => addToBrightlist(e, brightlistAddress)}
+                  >
+                    Brightlist
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div>
+              <h5>Revoke an ethereum address</h5>
+              <form>
+                <input
+                  type="text"
+                  value={revokeAddress || ""}
+                  required
+                  onChange={(e) => setRevokeAddress(e.target.value)}
+                />
+                <div>
+                  <button
+                    className="ml-button"
+                    onClick={(e) => revokeFromBrightlist(e, revokeAddress)}
+                  >
+                    Revoke
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div>
+              <h3>Create a Molecule NFT</h3>
               <form>
                 <div>Cure</div>
                 <input
@@ -205,10 +484,12 @@ export default function User() {
                     Mint token
                   </button>
                 </div>
-                <div>
-                  <button onClick={showKey()}>Show Encryption key</button>
-                </div>
               </form>
+              <div>
+                <button className="ml-button" onClick={showKey()}>
+                  Show Encryption key
+                </button>
+              </div>
             </div>
           </div>
         ) : (
